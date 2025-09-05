564f3a97f3414685e495d8d7b7fabace7dabcd27
priyanshu <priyanshukumari93933@gmail.com>
2025-09-05 11:56:33 +0530

    coaching-management-system

--- app/(superadmin)/admin/categories/page.jsx
+++ app/(superadmin)/admin/categories/page.jsx
@@ -3,11 +3,13 @@ import CategoryForm from '../ui/AddCategory'
 import CategoryList from '../ui/Categories'
 import connectDb from '@/app/utils/ConnectDb'
 import Category from '@/app/models/Category'
+export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
 
 const CategoryPage = async() => {
     await connectDb();
 
-    let categories=await Category.find()
+    let categories=await Category.find().lean()
   return (
     <div className='flex flex-col gap-5 w-full px-[10%]'>
         <h1 className='my-3 text-3xl font-semibold text-slate-600'>Category Management</h1>
